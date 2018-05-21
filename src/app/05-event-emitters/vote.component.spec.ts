663cb46de593fe345208f31513b4b2ee4d82b50e
fabio guelfi <fabioguelfiunix@gmail.com>
2018-05-21 09:27:37 -0300

    Working with Event Emitters

--- src/app/05-event-emitters/vote.component.spec.ts
+++ src/app/05-event-emitters/vote.component.spec.ts
@@ -1,19 +1,22 @@
 import { VoteComponent } from './vote.component'; 
 
+
 describe('VoteComponent', () => {
-  var component: VoteComponent; 
+
+  let component: VoteComponent;
 
   beforeEach(() => {
     component = new VoteComponent();
-  });
-
-  it('should raise votaChanged event when upvoted', () => {
+  })
+  
+  it('shoulde raise voteChange event when upvoted', () => {
     let totalVotes = null;
     component.voteChanged.subscribe(tv => totalVotes = tv);
 
     component.upVote();
 
-    // expect(totalVotes).not.toBeNull();
+    expect(totalVotes).not.toBeNull();
     expect(totalVotes).toBe(1);
   });
+
 });
