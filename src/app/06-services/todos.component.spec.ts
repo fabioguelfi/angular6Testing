654fb959bb3a296e44fb9d4e05a9b4213c69851a
fabio guelfi <fabioguelfiunix@gmail.com>
2018-05-21 13:18:48 -0300

    final

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -2,8 +2,11 @@ import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
 import { Observable } from 'rxjs';
 import 'rxjs/add/Observable/from';
+import 'rxjs/add/Observable/empty';
+import 'rxjs/add/observable/throw'
 
 describe('TodosComponent', () => {
+
   let component: TodosComponent;
   let service: TodoService;
 
@@ -12,16 +15,64 @@ describe('TodosComponent', () => {
     component = new TodosComponent(service);
   });
 
-  it('should set todos property with the items returned from the server', () => {
-    let todos = [1, 2, 3];
+  it('should set todos property with the items returned from webservice', () => {
+    let todos = [1, 2, 3]
 
-    spyOn(service, 'getTodos').and.callFake((() => {
-      return Observable.from([ todos ])
-    }));
+    spyOn(service, 'getTodos').and.callFake(() => {
+      return Observable.from([todos]);
+    })
 
     component.ngOnInit();
 
     expect(component.todos).toBe(todos);
+  });
+
+  it('should call the server to the save changes when a new todo', () => {
+    let spy = spyOn(service, 'add').and.callFake(t => {
+      return Observable.empty();
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should add the new todo returned from the server', () => {
+    let todo = { id: 1 };
+
+    let spy = spyOn(service, 'add').and.returnValue(Observable.from([todo]));
+
+    component.add();
+
+    expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+  });
+
+  it('should set the message property if server returns and error when adding a new todo', () => {
+    let error = 'error from the server'
+
+    let spy = spyOn(service, 'add').and.returnValue(Observable.throw(error));
+
+    component.add();
+
+    expect(component.message).toBe(error);
+  });
+
+  it('should call the server to delete a todo item if the user confirms', async () => {
+    spyOn(window, 'confirm').and.returnValue(true);
+    let spy = spyOn(service, 'delete').and.returnValue(Observable.empty());
+
+    component.delete(1);
+
+    expect(spy).toHaveBeenCalledWith(1)
+  });
+
+  it('should NOT call the server to delete a todo item if the user calcels', async () => {
+    spyOn(window, 'confirm').and.returnValue(false);
+    let spy = spyOn(service, 'delete').and.returnValue(Observable.empty());
+
+    component.delete(1);
+
+    expect(spy).not.toHaveBeenCalled();
   });
 
 });
